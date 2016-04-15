f8ef076679a6a4743b70dcad9870974c73c49959
sngjss <sunga.jss@gmail.com>
2016-04-15 17:05:46 -0400

    Updated links so that they may work on live server.

--- footer.php
+++ footer.php
@@ -14,8 +14,8 @@
 
             <div class="row" id="footer-top">
                <div class="medium-8 large-9 columns hide-for-small-only" id="footer-button">
-                  <a href="#" class="button"><img src="http://localhost:8888/wordpress/wp-content/uploads/2016/04/pawprint.png"/>Sponsor a Puppy</a>
-                  <a href="#" class="button"><img src="http://localhost:8888/wordpress/wp-content/uploads/2016/04/donate.png"/>Make a Donation</a>
+                  <a href="<?php echo site_url('/index.php/donate/'); ?>" class="button"><img src="http://localhost:8888/wordpress/wp-content/uploads/2016/04/pawprint.png"/>Sponsor a Puppy</a>
+                  <a href="<?php echo site_url('/index.php/donate/'); ?>" class="button"><img src="http://localhost:8888/wordpress/wp-content/uploads/2016/04/donate.png"/>Make a Donation</a>
                </div><!-- end #footer-button -->
                <div class="medium-4 large-3 columns" id="social-media">
                   <p>Get in touch with us:</p>
@@ -30,34 +30,34 @@
                   <div class="row">
                      <div class="medium-4 columns">
                         <ul>
-                           <li><a href="<?php echo site_url('/contact/'); ?>" class="section-title">Contact</a></li>
-                           <li><a href="<?php echo site_url('/contact/'); ?>">Mailing List</a></li>
-                           <li><a href="<?php echo site_url('/apply-volunteer/'); ?>">Volunteer</a></li>
+                           <li><a href="<?php echo site_url('/index.php/contact-us'); ?>" class="section-title">Contact</a></li>
+                           <li><a href="<?php echo site_url('/index.php/contact-us/'); ?>">Mailing List</a></li>
+                           <li><a href="<?php echo site_url('/index.php/apply/apply-volunteer/'); ?>">Volunteer</a></li>
                         </ul>
                         <ul>
-                           <li><a href="<?php echo site_url('/apply/'); ?>" class="section-title">Apply</a></li>
-                           <li><a href="<?php echo site_url('/apply-service-dog/'); ?>">Apply for a Service Dog</a></li>
-                           <li><a href="<?php echo site_url('/apply-release-dog/'); ?>">Apply for a Release Dog</a></li>                     
+                           <li><a href="<?php echo site_url('/index.php/apply/'); ?>" class="section-title">Apply</a></li>
+                           <li><a href="<?php echo site_url('/index.php/apply/apply-service-dog/'); ?>">Apply for a Service Dog</a></li>
+                           <li><a href="<?php echo site_url('/index.php/apply/apply-release-dog/'); ?>">Apply for a Release Dog</a></li>                     
                         </ul>
                      </div>
                      <div class="medium-4 columns">
                         <ul>
-                           <li><a href="<?php echo site_url('/about/'); ?>" class="section-title">About Us</a></li>
-                           <li><a href="<?php echo site_url('/programs/'); ?>">Our Programs</a></li>
-                           <li><a href="<?php echo site_url('/faq/'); ?>">FAQ</a></li>
-                           <li><a href="<?php echo site_url('/puppy-placement/'); ?>">Puppy Placement</a></li>
+                           <li><a href="<?php echo site_url('/index.php/about/'); ?>" class="section-title">About Us</a></li>
+                           <li><a href="<?php echo site_url('/index.php/about/programs/'); ?>">Our Programs</a></li>
+                           <li><a href="<?php echo site_url('/index.php/about/faq/'); ?>">FAQ</a></li>
+                           <li><a href="<?php echo site_url('/index.php/about/puppy-placement/'); ?>">Puppy Placement</a></li>
                         </ul>
                         <ul>
-                           <li><a href="<?php echo site_url('/awards/'); ?>" class="section-title">Awards and Publications</a></li>
-                           <li><a href="<?php echo site_url('/awards/'); ?>">PBB &amp; C2W Awards</a></li>
-                           <li><a href="<?php echo site_url('/awards/'); ?>">Publications</a></li>         
+                           <li><a href="<?php echo site_url('/index.php/awards/'); ?>" class="section-title">Awards and Publications</a></li>
+                           <li><a href="<?php echo site_url('/index.php/awards/'); ?>">PBB &amp; C2W Awards</a></li>
+                           <li><a href="<?php echo site_url('/index.php/awards/'); ?>">Publications</a></li>
                         </ul>                        
                      </div>
                      <div class="medium-4 columns">
                         <ul>
-                           <li><a href="<?php echo site_url('/media/'); ?>" class="section-title">Photos &amp; Videos</a></li>
-                           <li><a href="<?php echo site_url('/media/'); ?>">Holiday Cards</a></li>
-                           <li><a href="<?php echo site_url('/media/'); ?>">Videos</a></li>
+                           <li><a href="<?php echo site_url('/index.php/media/'); ?>" class="section-title">Photos &amp; Videos</a></li>
+                           <li><a href="<?php echo site_url('/index.php/media/'); ?>">Holiday Cards</a></li>
+                           <li><a href="<?php echo site_url('/index.php/media/'); ?>">Videos</a></li>
                         </ul>
                      </div>
                   </div>
