1e9fda7ea7114447a1ff5896f216dc35364367f2
kristinerobee <kristiner.gonzales@rocketmail.com>
2016-04-19 11:44:29 -0400

    Release Dog-Not finished
    Added contents, placeholder, and button that links to application from
    the actual website

--- page-templates/template-releaseDog.php
+++ page-templates/template-releaseDog.php
@@ -14,20 +14,21 @@
 
    do_action( 'ample_before_body_content' ); ?>
 
-    <div class="single-page clearfix">
-    	<div class="inner-wrap">
-            <div id="primary" class="content-box">
-               <div id="content">
-                  <?php while ( have_posts() ) : the_post(); ?>
-                     <?php get_template_part( 'content', 'page' ); ?>
-                  <?php endwhile; ?>
-               </div>
-               <?php ample_both_sidebar_select(); ?>
-            </div>
-            <?php ample_sidebar_select();hh ?>
-      	</div><!-- .inner-wrap -->
-  	</div><!-- .single-page -->
+   <div class="section group">
+		<div class="col span_2_of_6">
+			<img src="http://placehold.it/300x300" alt="release dog" id="releaseimg">
+		</div>
+		<div class="col span_4_of_6">
+			<div id="releasep">
+				<p>Since our standards for service dogs and explosive detection canines are so high, some dogs do not complete the program for either behavioral or health reasons .  These dogs will be released as pets for a charge of $4,000.00.   They will come to you spayed or neutered, current on all their vaccines, and incredibly bonded to humans.  The feedback we’ve gotten from everyone who has ever adopted one of our dogs is that they never knew that dogs could be as focused on people as are ours.
 
+				If you are interested in adopting one of our released dogs, please print </p>
+			</div>
+			<button type="button" id="button"><a href="http://www.puppiesbehindbars.com/files/pdfs/releasedpuppyapplication.pdf">Download the Release Dog<br>APPLICATION</a></button>
+		</div>
+		
+</div>
+	
    <?php
       if( is_active_sidebar( 'ample_business_sidebar' ) ) {
          // Calling the business sidebar if it exists.
